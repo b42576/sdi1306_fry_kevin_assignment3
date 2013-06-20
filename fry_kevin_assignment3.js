975d4c47a1693eade1a034d3ac9334ee62604f21
Kevin Fry <kevinfry@fullsail.edu>
2013-06-19 23:47:49 -0400

    json data added

--- fry_kevin_assignment3.js
+++ fry_kevin_assignment3.js
@@ -4,3 +4,28 @@
 // Custom JS app based on created flowchart
 
 
+var jsonData = {
+    "socialNetworks": [
+        {
+            "name": "Twitter",
+            "link": "https://twitter.com/",
+            "description": "Short \"text\" messages for the world to see."
+        },
+        {
+            "name": "Google+",
+            "link": "https://plus.google.com/",
+            "description": "Updates and shares from your circles of friends, family, co-workers, and people you admire."
+        },
+        {
+            "name": "LinkedIn",
+            "link": "https://www.linkedin.com/",
+            "description": "Professional networking."
+        },
+        {
+            "name": "Facebook",
+            "link": "https://www.facebook.com/",
+            "description": "Post messages, pictures, and videos for all your friends to see."
+        }
+    ]
+};
+
